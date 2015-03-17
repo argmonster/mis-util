5dee29c38d42f0c36f92657f09f85c93f94b33c3
Tim Markello <timmarkello@gmail.com>
2015-03-17 09:57:36 -0500

    ~ change parm line generation to remove any \r and \ns. MIS expects files to be 80 bytes wide with out any line ending control characters.

--- Parm/add_line.js
+++ Parm/add_line.js
@@ -1,5 +1,8 @@
 module.exports = function add_line(name, value) {
-   var head = name ? name.toUpperCase() + ' ' (value || '') : '';
-   var tail = Arra(80 - head.length).join(' ');
+   console.log('add', name, value);
+   name = name.replace(/[\r\n]/g, '');
+   value = (value || '').replace(/[\r\n]/g, '');
+   var head = name ? name.toUpperCase() + ' ' + value : '';
+   var tail = Array(80 - head.length).join(' ');
    return head + tail;
 };
