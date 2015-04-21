ff9b647b5b1281483fe439d0cf1468b12af3fd6a
Tim Markello <timmarkello@gmail.com>
2015-04-21 16:50:58 -0500

    ~ Change the module to export a function instead of object.    > pass in options to the function, then the function returns utility api with the options set.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Deploy = require('./Deploy/Deploy.js');
 var Parm = require('./Parm/Parm.js');
 
-module.exports = {
-   Deploy: Deploy,
-   Parm: Parm
+module.exports = function mis(options) {
+   return {
+      deploy: Deploy(options),
+      parm: Parm()
+   };
 };
