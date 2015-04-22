c29d0b119436bdd33702fa5bfbb61ee3707589c8
Tim Markello <timmarkello@gmail.com>
2015-04-22 11:38:40 -0500

    + too many changes ~ move connect and put functions out of ftp object and into closure ~ move the internal reference to the ftp connection to closure - the state of the ftp object does not include the ftp connection. a connection must be passed to utility functions + better fail support for changing directories

--- Deploy/Ftp.js
+++ Deploy/Ftp.js
@@ -5,88 +5,138 @@ var ftp = require('ftp');
 var get_source_files = require('./get_source_files.js');
 var clean_file_list = require('./clean_file_list.js');
 
-var Mis_ftp = function(options)  {
 
+var check_remote_path = function check_remote_path(f, path) {
+   var dfd = Q.defer();
+   f.cwd(path, function(err, dir) {
+      if (err) { 
+         dfd.reject(err);
+      }
+      dfd.resolve(dir)
+   });
+   return dfd.promise;
+};
+
+
+var make_remote_path = function make_report_path(f, path) {
+   var dfd = Q.defer();
+   f.mkdir(path, true, dfd.resolve);
+   return dfd.promise;
+};
+
+var connect = function connect(connectoptions) {
+   var dfd = Q.defer();
+   var f = new ftp();
+   f.on('ready', function() {
+      dfd.resolve(f);
+   });
+   f.on('error', function(err) {
+      dfd.reject(err);
+   });
+   console.log('connecting to', connectoptions.host, 'as', connectoptions.user);
+   f.connect(connectoptions);
+   return dfd.promise;
+};
+
+var put = function put(f, local_file, remote_dir, filename) {
+   console.log('putting file: ', local_file, 
+      'to', remote_dir, 
+      'as', filename);
+
+   var dfd = Q.defer();
+
+   check_remote_path(f, remote_dir)
+   
+   .then(function() {
+
+      f.put(local_file, filename, function(err) { 
+         if (err) { 
+            console.log(err); 
+            dfd.reject(err); 
+         }
+         dfd.resolve();
+      });
+
+   }).fail(function(err) { 
+      return dfd.reject(err);
+   });
+   
+   return dfd.promise;
+};
+
+var removeExtension = function(name) {
+   return name.substr(0, name.lastIndexOf('.')) || name;
+};
+
+var Mis_ftp = function(options)  {
    this.settings = options;
-   this.f = new ftp();
 };
 
 Mis_ftp.prototype = {
 
-   connect : function connect(connectoptions) {
-      var dfd = Q.defer();
-      var self = this;
-      self.f.on('ready', dfd.resolve);
-      self.f.connect(connnectoptions);
-      return dfd.promise;
-   },
-
-   check_remote_path : function check_remote_path(path) {
-      var dfd = Q.defer();
-      var self = this;
-      self.f.cwd(path, function(err, dir) {
-         if (err) { 
-            dfd.reject(new Error(err));
-         }
-         dfd.resolve(dir)
-      });
-      return dfd.promise;
-   },
-
-   make_remote_path : function make_report_path(path) {
-      var dfd = Q.defer();
-      var self = this;
-      self.f.mkdir(path, true, dfd.resolve);
-      return dfd.promise;
-   },
 
    push : function push(local_file, remote_dir) {
       var dfd = Q.defer();
       var self = this;
+      var f;
 
-      var filename = local_file.split('/').pop();
+      var filename = local_file.split('/')
+         .pop();
 
-      console.log(remote_dir);
-      self.check_remote_path(remote_dir)
-      .then(function() {
-         console.log('putting file: ', local_file, remote_dir, filename.replace(/\.[^.]*$/, ''));
-         self.f.put(local_file, remote_dir + filename.replace(/\.[^.]*$/, ''), function(err) { 
-            if (err) { console.log(err); dfd.reject(err); }
-            dfd.resolve();
-         });
+      filename = removeExtension(filename); 
+
+      connect(this.settings.connect, filename)
+      .fail(function(err) {
+         console.log('connection fail' , err);
       })
-      .fail(
-         function(err) { 
-            console.log('fail', err);
-            dfd.reject(err);
+      .then(function(conn) {
+         f = conn;
+         console.log('call put ', filename, f.connected);
+         return put(f, local_file, remote_dir, filename)
+      })
+      .then(function() {
+         console.log('done ', filename);
+         dfd.resolve();
+      })
+      .fail(function(err) { 
+         console.log('fail ', filename, err);
+         dfd.reject(err);
+      })
+      .finally(function() {
+         console.log('close', filename);
+         f.end();
+         //return dfd;
       });
+
       return dfd.promise;
    },
 
    push_dir : function(local, remote, extension) {
 
       var dfd = Q.defer();
-      var self = this;
+
+      var f, list;
+      var connectoptions = this.settings.connect;
 
       get_source_files(local)
-      .then(function(files) { 
-         var list = clean_file_list(files, extension);
-
-         self.f.on('ready', function() {
-            console.log('connected, pushing files to: ' + remote);
-            var pushed = list.map(function(file) {
-               console.log(local + file);
-               console.log(self.settings.sysname, ' remote: ', remote);
-               return self.push(local + file, self.settings.sysname + remote);
-            });
-
-            Q.when(pushed).then(function() {
-               console.log('Push Complete');
-               self.f.end();
-            });
-         });
-         self.f.connect(self.settings.connect);
+      .then(function(files) {
+         list = clean_file_list(files, extension);
+         return connect(connectoptions);
+      })
+      .then(function(conn) {
+         f = conn;
+         return Q.all(list.map(function(file) {
+            return put(f, local + file, remote, removeExtension(file));
+         }));
+         
+      })
+      .then(function() {
+         console.log('done', local);
          dfd.resolve();
+      })
+      .finally(function() {
+         console.log('close',local);
+         f.end();
       });
 
       return dfd.promise;
