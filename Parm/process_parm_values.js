439b8a376de313a42b4a161776a8ea367ecadd30
Tim Markello <timmarkello@gmail.com>
2015-04-22 11:49:39 -0500

    + add support for defining parm values that are arrays, and parm values that are arrays of arrays.    > array values will map to key-# value, where # is the index of the array with the value of 'value'    > array of arrays will map to key-#-# value where #-# is the row and column of the table with the value of 'value' + array values and array of array values are flattened so that the parm line output is a 1 dementional array 80 byte length strings

--- Parm/process_parm_values.js
+++ Parm/process_parm_values.js
@@ -1,9 +1,16 @@
+var add_line = require('./add_line.js');
+
 module.exports = function process_parm_values(key, val) {
    if (val instanceof Array) {
-      return val.map(function(v, i) {
-         return add_line(key +'-' + i, v);
-      });
-   } else if (val !instanceof Object) {
+      return Array.prototype.concat.apply([], val.map(function(v, i) {
+         if (v instanceof Array) {
+            return v.map(function(curr, j) {
+               return add_line(key + '-' + (i + 1) + '-' + (j + 1), curr);
+            });
+         }
+         return [add_line(key +'-' + (i + 1), v)];
+      }));
+   } else if (!(val instanceof Object)) {
       return [add_line(key, val)];
    } else {
       console.log('Unsupported value for key: ', key);
