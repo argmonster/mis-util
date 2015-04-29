3c070b92128d33a902ab4d5ff8d6c5aae9a9d7de
Tim Markello <timmarkello@gmail.com>
2015-04-29 16:50:31 -0500

    + add sugar functions on Deploy object to push js, css and img files to remote bui server

--- Deploy/Deploy.js
+++ Deploy/Deploy.js
@@ -4,12 +4,19 @@ var Ftp = require('./Ftp.js');
 
 var defaults = {
    sysname: '/c1/FRSH',
+   webname: '/c0/cmhcweb/cmhcbui/cmhcbuilocal/',
    usc_path: {local: './usc/', remote: '/SCRIPT/S'},
    view_path: {local: './views/', remote: '/CUST/forms'},
    parm_path: {local: './parm/', remote: '/PARM'},
+   js_path: {local: './js/', remote: '/js'},
+   css_path: {local: './css/', remote: '/css'},
+   img_path: {local: './img/', remote: '/img'},
    usc_extension: 'usc',
    view_extension: 'uhp|html|htm',
    parm_extension: 'parm',
+   js_extension: 'js',
+   css_extension: 'css',
+   img_extension: 'jpg|png|gif',
    connect: {
       //host: 'hostname',
       //user: 'username'
@@ -29,7 +36,7 @@ module.exports = function(options) {
          settings.sysname + settings.usc_path.remote, 
          settings.usc_extension
       ),
-      views: ftp.push_dir.bind(
+      view: ftp.push_dir.bind(
          ftp, 
          settings.view_path.local, 
          settings.sysname + settings.view_path.remote, 
@@ -40,6 +47,24 @@ module.exports = function(options) {
          settings.parm_path.local, 
          settings.sysname + settings.parm_path.remote, 
          settings.parm_extension
+      ),
+      js: ftp.push_dir.bind(
+         ftp,
+         settings.js_path.local,
+         settings.webname + 
+            settings.sysname.replace('/', '') + 
+            settings.js_path.remote,
+         settings.js_extension,
+         true
+      ),
+      css: ftp.push_dir.bind(
+         ftp,
+         settings.css_path.local,
+         settings.webname + 
+            settings.sysname.replace('/', '') + 
+            settings.css_path.remote,
+         settings.css_extension,
+         true
       )
    };
 };
