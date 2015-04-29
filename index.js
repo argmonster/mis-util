91c3cc50201cc38bb3325d9e198703ee6162ff40
Tim Markello <timmarkello@gmail.com>
2015-04-29 14:12:59 -0500

    + add options, defaults and settings at the root object so that common settings can be used 'globally'

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+var _ = require('lodash');
 var Deploy = require('./Deploy/Deploy.js');
 var Parm = require('./Parm/Parm.js');
 
+var defaults = {
+   connect: {
+      //host: 'hostname',
+      //user: 'user',
+      //password: 'pass'
+   }
+};
+
 module.exports = function mis(options) {
+   var settings = _.merge(defaults, options);
    return {
-      deploy: Deploy(options),
+      deploy: Deploy(settings),
       parm: Parm()
    };
 };
