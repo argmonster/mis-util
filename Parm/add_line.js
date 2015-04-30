af61d6cc6b7e5feb29c23eb9ad95037fd5656320
Tim Markello <timmarkello@gmail.com>
2015-04-30 10:07:28 -0500

    ^ fix add_line function so that the line returned is 80 characters long

--- Parm/add_line.js
+++ Parm/add_line.js
@@ -2,6 +2,6 @@ module.exports = function add_line(name, value) {
    name = name.replace(/[\r\n]/g, '');
    value = (value || '').replace(/[\r\n]/g, '');
    var head = name ? name.toUpperCase() + ' ' + value : '';
-   var tail = Array(80 - head.length).join(' ');
+   var tail = Array(81 - head.length).join(' ');
    return head + tail;
 };
