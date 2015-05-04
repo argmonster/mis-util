e9dac3f62a5f7e249c6d4c6ca51f9b9f9fd0770e
Tim Markello <timmarkello@gmail.com>
2015-05-04 14:56:25 -0500

    + import.dst function for importing dsts into the target system.    > dsts are imported form specified file, and should confirm to the file formate specified by the import-dsts script.    > file is ftped to remote server 'import directory' specified in the settings object. (defaults to '/c1/import')    >import-dsts script is then executed

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,12 +1,16 @@
 var _ = require('lodash');
 
 var remote = require('./Remote.js');
+var Ftp = require('../Deploy/Ftp.js');
 
-var defaults = {};
+var defaults = {
+   importdir: '/c1/import'
+};
 var settings = {};
 
 module.exports = function(options) {
    settings = _.merge({}, defaults, options);
+   var ftp = new Ftp(settings);
    return {
       run: remote.runscript.bind(remote, 
          settings.connect,
@@ -25,6 +29,24 @@ module.exports = function(options) {
                return {name: 'compile', args: [script]};
             })
          );
+      },
+      import: {
+         dst: function(dstfile) {
+            return ftp.push(dstfile, settings.importdir)
+            .then(function(filename) {
+               return remote.runscript(
+                  settings.connect,
+                  settings.sysname,
+                  settings.parm_path.remote,
+                  settings.cron.user,
+                  settings.cron.pass,
+                  [{name: 'import-dsts', 
+                     args: [settings.importdir + '/' + filename]
+                  }]
+               );
+            });
+         }
+
       }
    };
 };
