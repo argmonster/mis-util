6b68712140d2aa92cc82834e74b4775e021f9a3c
Tim Markello <timmarkello@gmail.com>
2015-07-14 12:33:33 -0500

    + add Q.js dependency + new settings    : dstscript - local path to the dst import script source file    : compilescript - local path to the compile script _OBJECT CODE_ file
    + new exposed functions
       : install(script) - installs the specified script's source code and compiles the script. This function installs the compile object code into the target system prior to compiling the code, then removes upon completion.
          -> returns a promise which resolves to the installed script's name
       : installexecutable(file) - installs the specified file into the target systems script object code directory. installed file is expected to be CMHC uscript executable object code
       : uninstallexecutable(file) - removes the specified script from the target's script object directory
       : uninstall(script) - removes the specified script from the target's script source AND object directories.
       : runonce(script, scriptargs) - installs the specified script, and runs the script once with the specified arguments, then uninstalls the script
    
    ~ updates to existing function
       : compile([scripts]) - function now returns a promise that resolves when all the specified scripts have compiles
       : import.dst(dstfile) - updated this function to use the new 'runonce' function. Requires the dstscript setting. Also removes the dstfile from the target system upon completion.
    
    modified:   Script.js

--- Script.js
+++ Script.js
@@ -1,91 +1,140 @@
 var _ = require('lodash');
+var q = require('q');
 
 var remote = require('./libs/Remote.js');
 var Ftp = require('./libs/Ftp.js');
 
 var defaults = {
-   importdir: '/c1/import'
+   importdir: '/c1/import',
+   dstscript: './usc/import-dsts.usc',
+   compilescript: './usc/compile'
 };
 var settings = {};
 
 module.exports = function(options) {
    settings = _.merge({}, defaults, options);
    var ftp = new Ftp(settings);
-   var self = this;
-   return {
+   var api = {
       install: function(script) { 
-         return ftp.push(script, settings.usc_path.remote)
-         .then(return self.compile.bind(self, filename)),
-
+         console.log('Installing', script);
+         return ftp.push(script, [
+            settings.sysname,
+            settings.usc_path.remote
+            ].join(''))
+         .then(function(usc) { 
+            console.log('COMPILING::', usc);
+            return api.installexecutable(settings.compilescript)
+            .then(function(compilescript) {
+               return api.compile([usc])
+               .then(function() {
+                  return api.uninstallexecutable(compilescript);
+               });
+            })
+            .then(function() {return q(usc)});
+         })
+         .then(function(usc) {
+            console.log("COMPILE COMPLETE", usc);
+            return q(usc);
+         });
+      },
+      installexecutable: function(file) {
+         console.log('Installing Object Code', file);
+         return ftp.push(file, [
+            settings.sysname,
+            settings.usc_path.remote.replace(/\/S$/, '/O/')
+            ].join(''));
+      },
+      uninstallexecutable: function(file) {
+         console.log('UnInstalling Object Code', file);
+         return remote.removefile(
+            settings.connect,
+            settings.sysname,
+            settings.usc_path.remote.replace(/\/S$/, '/O'),
+            file);
+      },
+      uninstall: function(script) {
+         console.log('Uninstalling ', script);
+         var cmd = [
+            "rm ",
+            settings.sysname,
+            settings.usc_path.remote,
+            "/", 
+            script,
+            ' ',
+            settings.sysname,
+            settings.usc_path.remote.replace(/\/S$/,'/O'),
+            '/',
+            script
+         ].join('');
+         return remote.execute(settings.connect, cmd);
+      },
       run: remote.runscript.bind(remote, 
          settings.connect,
          settings.sysname, 
          settings.parm_path.remote,
          settings.cron.user, 
          settings.cron.pass),
+
       runonce: function(script, scriptargs) {
          scriptargs = scriptargs || [];
-         return ftp.push(script, settings.usc_path.remote)
-         .then(function(filename) {
-            return remote.runscript(
-               settings.connect,
-               settings.sysname,
-               settings.parm_path.remote,
-               settings.cron.user,
-               settings.cron.pass,
-               [{name: filename,
-                  args: scriptargs
-               }]
-            );
-         })
-         .then(function(filename) {
-            return remote.execute(settings.connect,
-               'rm ' + settings.usc_path.remote + '/' + filename);
-         });
-      },
-
-      compile: function(scripts) { 
-         return remote.runscript(
-            settings.connect,
-            settings.sysname, 
-            settings.parm_path.remote,
-            settings.cron.user, 
-            settings.cron.pass, 
-            scripts.map(function(script) {
-               return {name: 'compile', args: [script]};
-            }))
-            .then(function(conn) {
-               console.log('COMPILED CHECKING RESULTS');
-               console.log(scripts);
-               scripts.forEach(function(script) {
-                  var path = settings.sysname + '/W/' + script + '.log';
-                  var cmd = 'grep ERRLINE ' + path;
-                  remote.execute(settings.connect, cmd)
-                  .then(function(conn) {
-                     cmd = 'rm ' + path
-                     return remote.execute(settings.connect, cmd);
-                  });
-               });
+         return api.install(script)
+         .then(function(usc) { 
+            return api.run([{name: usc, args: scriptargs}])
+            .then(function() {
+               api.uninstall(usc)
             });
-         //);
+         })
+      },
+      compile: function(scripts) { 
+         return api.run(
+         scripts.map(function(script) {
+            console.log('mapping: ', script);
+            return {name: 'compile', args: [script]};
+         }))
+         .then(function(conn) {
+            console.log('COMPILED CHECKING RESULTS');
+            console.log(scripts);
+            return q.all(scripts.map(function(script) {
+               var path = [
+                  settings.sysname,
+                  '/W/',
+                  script,
+                  '.log'
+                  ].join('');
+               var cmd = [
+                  'grep ERRLINE',
+                  path
+                  ].join(' ');
+               return remote.execute(settings.connect, cmd)
+               .then(function(conn) {
+                  cmd = [
+                     'rm',
+                     path
+                  ].join(' '); 
+                  return remote.execute(settings.connect, cmd);
+               });
+            }));
+         });
       },
       import: {
          dst: function(dstfile) {
             return ftp.push(dstfile, settings.importdir)
             .then(function(filename) {
-               return remote.runscript(
-                  settings.connect,
-                  settings.sysname,
-                  settings.parm_path.remote,
-                  settings.cron.user,
-                  settings.cron.pass,
-                  [{name: 'import-dsts', 
-                     args: [settings.importdir + '/' + filename]
-                  }]
-               );
-            });
+               return api.runonce(settings.dstscript, [ 
+                  [settings.importdir, filename].join('/')
+               ])
+               .then(function() {
+                  var cmd = [
+                     "rm ",
+                     settings.importdir,
+                     "/",
+                     filename
+                  ].join('');
+                  return remote.execute(settings.connect, cmd);
+               })
+            })
          }
-
       }
    };
+   return api;
 };
